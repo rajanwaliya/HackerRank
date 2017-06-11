0900606156f4b2f7cffed909e211034ad1cee4ad
rajanwaliya <rajanwaliya@gmail.com>
2017-06-11 19:59:59 +0530

    added comments for jimandorders

--- Algorithms/Greedy/JimAndOrders.cpp
+++ Algorithms/Greedy/JimAndOrders.cpp
@@ -1,17 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 class order{
     public:
     int index;
     int time;
     
+    //giving parameter names index and time didn't work, hence i and t
     order(int i,int t){
         index = i;
         time = t;
     }
 };
-
+//first sort by ascending time, if same time, then by ascending index
+/**
+hint: (1)if don't want to remember sign for ascending and desc, print after sorting, and flip the sign if not sorted
+as desired
+(2) if want to remember sign: after sorted in asending first<second hence o1.time < o2.time
+**/
 bool compare(order o1, order o2){
     if(o1.time!=o2.time){
         return o1.time < o2.time;
@@ -21,7 +28,6 @@ bool compare(order o1, order o2){
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n;
     cin >> n;
     vector<order> v;
