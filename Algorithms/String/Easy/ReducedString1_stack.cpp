0c1fbd4a839f0055f1ba97a32f73726e0e40b310
rajanwaliya <rajanwaliya@gmail.com>
2017-06-15 08:58:39 +0530

    modified comments in reduced string

--- Algorithms/String/Easy/ReducedString1_stack.cpp
+++ Algorithms/String/Easy/ReducedString1_stack.cpp
@@ -6,18 +6,22 @@ int main() {
     cin >> s;
     stack<char> st;
     for(int i=0;i<s.size();i++){
-        //if recently saw someone to pair with current char
+        //if no match on top of stack
         if(st.empty()||st.top()!=s[i]){
             st.push(s[i]);
-        }else{ //keep this in the waiting
+        }else{ //if top matches current char, pop it (also not pushing the current char)
+            //hence removing the pair
             st.pop();
         }
     }
     string ans = "";
+    
     while(!st.empty()){
         ans+=st.top();
         st.pop();
     }
+
+    //why reverse?
     reverse(ans.begin(),ans.end());
     cout << ans ;
     if(ans==""){
