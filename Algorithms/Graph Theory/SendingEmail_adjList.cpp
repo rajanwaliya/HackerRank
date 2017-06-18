04c881df1dccfaeefb1a52ddbdd3217a73d7e758
rajanwaliya <rajanwaliya@gmail.com>
2017-06-18 22:36:19 +0530

    graph theory3

--- Algorithms/Graph Theory/SendingEmail_adjList.cpp
+++ Algorithms/Graph Theory/SendingEmail_adjList.cpp
@@ -2,27 +2,30 @@
 using namespace std;
 
 # define INF 0x3f3f3f3f
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
 
 int main() {
 	int N,n,m,s,t;
 	cin  >> N;
-	int cs = 0; //'case' is a keyword, used in switch-case
+	int cases = 0; //'case' is a keyword, used in switch-case
 	while(N-->0){
-		cs++;
-		cin >>n >> m >> s >> t;
+		scanf("%d %d %d %d",&n,&m,&s,&t);
 		vector<pair<int,int>> g[n];
 		int x,y,z;
 		while(m-->0){
-			cin >> x >>y >> z;
-			g[x].push_back(make_pair(y,z));
-			g[y].push_back(make_pair(x,z));
+			scanf("%d %d %d", &x, &y, &z);
+			g[x].push_back(ii(y,z));
+			g[y].push_back(ii(x,z));
 		}
 		
 		//dijkstra's from src to target
 		vector<int> dis(n,INF);
 		dis[s] = 0;
-		priority_queue<pair<int,int>> pq;
-		pq.push(make_pair(dis[s],s));
+		priority_queue<ii, vector<ii>, greater<ii> > pq;
+		pq.push(ii(dis[s],s));
 		int curNode;
 		int curDis;
 		int nbNode;
@@ -30,27 +33,30 @@ int main() {
 		while(!pq.empty()){
 			curNode = pq.top().second;
 			curDis = pq.top().first;
-			if(dis[curNode]<curDis){
+			pq.pop();
+			
+			if(dis[curNode]!=curDis){
 				continue;
 			}
-			pq.pop();
 			for(int i=0;i<g[curNode].size();i++){
-				nbNode = g[curNode][i].first;
-				nbW = g[curNode][i].second;
+				nbNode = g[curNode][i].second;
+				nbW = g[curNode][i].first;
 				//dis of neighbor < curNode dis + edge weight
 				if(dis[nbNode] > dis[curNode] + nbW){
 					dis[nbNode] = dis[curNode] + nbW;
-					pq.push(make_pair(dis[curNode] + nbW,nbNode));
+					pq.push(ii(dis[curNode] + nbW,nbNode));
 				}
 			}
-			if(curNode==t){break;}
+			//if(curNode==t){break;}
 		}
-		cout << "Case#" <<cs<<": ";
-		if(dis[t]==INF){
-			cout << "ureachable" << endl;
-		}else{
-			cout << dis[t] << endl;
+
+		if(dis[t]!=INF){
+            printf("Case #%d: %d\n",cases++,dis[t]);
+        }else{
+            printf("Case #%d: unreachable\n",cases++);
 		}
+
+
 	}
 	return 0;
 }
